4157974b358b37810dbcb3816c14e757a9a2b2d7
Andy Lam <andlam@microsoft.com>
2020-04-14 18:40:37 -0700

    git changes

--- powershell/modules/GitHelpers/GitHelpers.psm1
+++ powershell/modules/GitHelpers/GitHelpers.psm1
@@ -5,16 +5,40 @@ function Get-GitStatus {
     $rawStatus = @(git status --porcelain)
     foreach ($status in $rawStatus) {
         @{
-            RepoPath = $status.Substring(3)  -replace "/","\";
-            IndexStatus = $status[0];
-            TreeStatus = $status[1]
+            RepoPath = $status.Substring(3) -replace "/","\";
+            Status = if ($status[0] -ne " ") {$status[0]} else {$status[1]};
+        }
+    }
+}
+
+function Get-GitDiffFiles {
+    param(
+        $lookback = 0
+    )
+    if ($lookback -gt 0) {
+        $revision = "HEAD~$lookback"
+    } else {
+        $revision = "HEAD"
+    }
+
+    $rawDiffFiles = git diff $revision --name-status
+    foreach ($rawDiffFile in $rawDiffFiles) {
+        $entry = $rawDiffFile -split "\s+";
+        @{
+            RepoPath = $entry[1] -replace "/","\";
+            Status = $entry[0]
         }
     }
 }
 
 function Get-GitModifiedFiles {
-    $modifiedFlags = "M", "A"
+    param (
+        $lookback = 0
+    )
+    # get untracked files from git status
     Get-GitStatus | 
-    Where-Object { $_.IndexStatus -in $modifiedFlags -or $_.TreeStatus -in $modifiedFlags } |
-    Select-Object { $_.RepoPath }
+    Where-Object { $_.Status -eq "?" } |
+    ForEach-Object { $_.RepoPath }
+    Get-GitDiffFiles $lookback |
+    ForEach-Object { $_.RepoPath }
 }
