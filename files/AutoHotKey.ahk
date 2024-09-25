7d78fafafbb4e86a08a41bad2d02290a4317c315
Andy Lam <lam.andy.tr@outlook.com>
2024-09-25 15:21:24 -0700

    updated setup and migrate to AHK 2

--- files/AutoHotKey.ahk
+++ files/AutoHotKey.ahk
@@ -1,7 +1,8 @@
 #SingleInstance force
 
 ; Ctrl would result in WinKey+Ctrl mask conflicting with Windows Speech Recognition.
-#MenuMaskKey vk07
+; TODO: No longer relevant with AHK 2.0?
+; #MenuMaskKey vk07
 
 ; Common Key for sendinput
 ; !{key} -> Alt + {key}
@@ -12,34 +13,38 @@
 #Include *i %A_MyDocuments%\AutoHotKeyU64_WorkSpecific.ahk
 
 #j::
-  Run notepad "%A_MyDocuments%\AutoHotKey.ahk" ; "C:\Program Files (x86)\Vim\vim74\gvim.exe"
+{
+  Run "notepad" "%A_MyDocuments%\AutoHotKey.ahk" ; "C:\Program Files (x86)\Vim\vim74\gvim.exe"
   Sleep 5000
-  WinActivate, AutoHotKey.ahk - Notepad
-return
+  WinActivate "AutoHotKey.ahk - Notepad"
+}
 
 #u::
-  Run notepad "%A_MyDocuments%\AutoHotKeyU64_WorkSpecific.ahk" ; "C:\Program Files (x86)\Vim\vim74\gvim.exe"
+{
+  Run "notepad" "%A_MyDocuments%\AutoHotKeyU64_WorkSpecific.ahk" ; "C:\Program Files (x86)\Vim\vim74\gvim.exe"
   Sleep 5000
-  WinActivate, AutoHotKey
-return
+  WinActivate "AutoHotKey"
+}
 
 #k::
+{
   Reload
   Sleep 2000 ; If successful, the reload will close this instance during the Sleep, so the line below will never be reached.
-  MsgBox, 4,, The script could not be reloaded. Would you like to open it for editing?
-  IfMsgBox, Yes, Edit
-return
+  response := MsgBox ("The script could not be reloaded. Would you like to open it for editing?","",4)
+  if (response = "Yes")
+    Edit
+}
 
-#t::Run,C:\Program Files\ConEmu\ConEmu64.exe -run {Shells::PowerShell}
-#+t::Run,C:\Program Files\ConEmu\ConEmu64.exe -run {Shells::PowerShell (Admin)}
-#y::Run, *RunAs wt
+#t::Run "C:\Program Files\ConEmu\ConEmu64.exe -run {Shells::PowerShell}"
+#+t::Run "C:\Program Files\ConEmu\ConEmu64.exe -run {Shells::PowerShell (Admin)}"
+#y::Run '*RunAs wt'
 
-#s::Run, *RunAs "C:\Program Files (x86)\FileZilla Server\FileZilla Server.exe" /start
-#+s::Run, *RunAs "C:\Program Files (x86)\FileZilla Server\FileZilla Server.exe" /stop
+#s::Run '*RunAs "C:\Program Files (x86)\FileZilla Server\FileZilla Server.exe" "/start"'
+#+s::Run '*RunAs "C:\Program Files (x86)\FileZilla Server\FileZilla Server.exe" "/stop"'
 
 ; Remap the windows movement to only require ctrl and not the winkey
-;#9::SendInput,{Ctrl down}{LWin down}{Left}{Ctrl up}{LWin up}
-;#0::SendInput,{Ctrl down}{LWin down}{Right}{Ctrl up}{LWin up}
+;#9::SendInput {Ctrl down}{LWin down}{Left}{Ctrl up}{LWin up}
+;#0::SendInput {Ctrl down}{LWin down}{Right}{Ctrl up}{LWin up}
 ;!Left::^#Left
 ;!Right::^#Right
 Alt & 0::AltTab
@@ -49,14 +54,14 @@ Alt & 9::ShiftAltTab
 
 Alt & -::AltTabMenuDismiss
 
-Alt & y::SendInput y{Enter}
-Alt & n::SendInput n{Enter}
+Alt & y::SendInput "y{Enter}"
+Alt & n::SendInput "n{Enter}"
 
-!Numpad0::SendInput {Media_Play_Pause}
-!NumpadIns::SendInput {Media_Play_Pause}
-!Numpad4::SendInput {Media_Prev}
-!NumpadLeft::SendInput {Media_Prev}
-!Numpad6::SendInput {Media_Next}
-!NumpadRight::SendInput {Media_Next}
-!NumpadAdd::SoundSet +5
-!NumpadSub::SoundSet -5
+!Numpad0::SendInput "{Media_Play_Pause}"
+!NumpadIns::SendInput "{Media_Play_Pause}"
+!Numpad4::SendInput "{Media_Prev}"
+!NumpadLeft::SendInput "{Media_Prev}"
+!Numpad6::SendInput "{Media_Next}"
+!NumpadRight::SendInput "{Media_Next}"
+!NumpadAdd::SoundSetVolume +5
+!NumpadSub::SoundSetVolume -5
--- setupwindows.ps1
+++ setupwindows.ps1
@@ -33,58 +33,50 @@ function RefreshPath {
 
 function CreateShortcut {
     param (
+        $shortcutFile,
         $targetFile,
-        $shortcutFile
+        $shortcutArgs
     )
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($shortcutFile)
     $Shortcut.TargetPath = $targetFile
+    if ($shortcutArgs) {
+        $Shortcut.Arguments = $shortcutArgs
+    }
     $Shortcut.Save()
 }
 
-if (-not $(Get-Command Choco*)) {
-    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+function WingetInstall {
+    param (
+        $searchType,
+        $appname
+    )
+    winget install --accept-source-agreements --accept-package-agreements --source winget "--$searchType" $appname
 }
 
-choco feature enable -n allowGlobalConfirmation
+#Update winget
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-choco install vcredist140
-choco install microsoft-windows-terminal
-
-choco install firefox
-
-choco install notepadplusplus.install
-
-choco install python
-
-#choco install 7zip.install
-# install 7z via installer so we get the context menu items
-function Get7z {
-    Write-Output "Get7z"
-    $exePath = "$env:USERPROFILE\Downloads\7zx64.installer.exe"
-    TryDownload "https://d3.7-zip.org/a/7z2107-x64.exe" $exePath
-
-    & $exePath /S
-    # MSIExec returns immediately so need to wait
-    WaitForAllOf "7zx64.installer"
-}
-if (-not (Test-Path "C:\Program Files\7-Zip")) {
-    Get7z
-}
+WingetInstall id Microsoft.Sysinternals.ProcessMonitor
+WingetInstall id Mozilla.Firefox
+WingetInstall id Notepad++.Notepad++
+WingetInstall id 7zip.7zip
+WingetInstall moniker Python3
+WingetInstall id AutoHotkey.AutoHotkey
+WingetInstall id VideoLAN.VLC
+WingetInstall id Git.Git
+WingetInstall id Microsoft.VisualStudioCode
 
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
 
-choco install vlc
-
-choco install git.install
-
-choco install vscode
-# Install extensions
+# Install VSCode extensions
 RefreshPath
 Invoke-Command { code --install-extension ms-vscode.powershell } -ErrorAction SilentlyContinue
 Invoke-Command { code --install-extension ms-vscode.cpptools } -ErrorAction SilentlyContinue
 Invoke-Command { code --install-extension streetsidesoftware.code-spell-checker } -ErrorAction SilentlyContinue
 
+<#
 
 if (-not (Get-Command vim -ErrorAction SilentlyContinue)) {
     choco install vim
@@ -93,12 +85,15 @@ if (-not (Get-Command vim -ErrorAction SilentlyContinue)) {
     #git clone https://github.com/sickill/vim-monokai.git $env:USERPROFILE\vimfiles\colors\monokai_repo
     #Copy-Item $env:USERPROFILE\vimfiles\colors\monokai_repo\colors\*  $env:USERPROFILE\vimfiles\colors\
 }
+#>
 
-choco install autohotkey
 $ahkShortcutPath = "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\autohotkey.lnk"
 if (-not (Test-Path $ahkShortcutPath)) {
-    CreateShortcut 'C:\Program Files\AutoHotkey\AutoHotkey.exe' $ahkShortcutPath
+    CreateShortcut  $ahkShortcutPath 'C:\Program Files\AutoHotkey\v2\AutoHotkey64.exe' " $env:USERPROFILE\Documents\Autohotkey.ahk"
 }
+
+
+
 
 
 function GetDotfileRepo {
@@ -117,11 +112,13 @@ if (-not (Test-Path "$env:USERPROFILE\Source\Repos\dotfiles")) {
 }
 
 # Remove Windows junk
+<#
 $keep = "(Microsoft|Windows|NVIDIA|Realtek|Intel|AMD|PaloAltoNetworks)";
 $apps = (Get-AppxPackage | ? { $_.PackageFullName -notmatch "^$keep|\.$keep" -and $_.Publisher -notmatch "$keep" })
 Write-Output "Removing apps:"
 ($apps).PackageFullName
 $apps | Remove-AppPackage
+#>
 
 # For registry keys, ignore delete failures
 $ErrorActionPreference = "Continue"
@@ -147,8 +144,21 @@ reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v
 #show suggestions in start
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SubscribedContent-338381Enabled /t REG_DWORD /d 0 /f
 
+#show content on lock screen
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v RotatingLockScreenEnabled /t REG_DWORD /d 0 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SubscribedContent-338380Enabled /t REG_DWORD /d 0 /f
+
+$sid = (([Security.Principal.WindowsIdentity]::GetCurrent()).User.Value)
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\$sid" /v RotatingLockScreenEnabled /t REG_DWORD /d 0 /f
+
+
+
 #show recently opened items in start/quick access
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Start_TrackDocs /t REG_DWORD /d 0 /f
+#start notifications and reccomendations
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Start_AccountNotifications /t REG_DWORD /d 0 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Start_IrisRecommendations /t REG_DWORD /d 0 /f
+
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
 #badges on taskbar buttons
@@ -197,11 +207,15 @@ reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hide
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CabinetState /v FullPath /t REG_DWORD /d 1 /f
 # show drive letters
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer /v ShowDriveLettersFirst /t REG_DWORD /d 0 /f
-    # edge tabs on alt-tab
-    reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v MultiTaskingAltTabFilter /t REG_DWORD /d 3 /f
+# edge tabs on alt-tab
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v MultiTaskingAltTabFilter /t REG_DWORD /d 3 /f
 #cortana
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowCortanaButton /t REG_DWORD /d 0 /f
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
+
+# dont show recents on start
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Start /v ShowRecentList /t REG_DWORD /d 0 /f
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Start_Layout /t REG_DWORD /d 0 /f
 
 #Taskbar settings
     # news on taskbar
@@ -214,6 +228,9 @@ reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Task
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarSh /t REG_DWORD /d 0 /f
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarSn /t REG_DWORD /d 0 /f
 
+#disable Dynamic Lighting - ambient lighting
+reg add HKCU\Software\Microsoft\Lighting /v AmbientLightingEnabled /t REG_DWORD /d 0 /f
+reg add HKCU\Software\Microsoft\Lighting /v ControlledByForegroundApp /t REG_DWORD /d 0 /f
 
 
 
